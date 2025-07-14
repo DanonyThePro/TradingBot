b34c87438f2bd9a7fc441571ba35ec4a7030a23a
DanonyThePro <139066048+DanonyThePro@users.noreply.github.com>
2025-07-14 23:04:35 +0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -23,7 +23,7 @@ def status():
     return render_template('status.html', status=status_data["status"], symbol=status_data["symbol"], current_price=status_data["Current Price"], entry_price=status_data["Entry Price"], stop_loss=status_data["Stop Loss"], take_profit=status_data["Take Profit"], balance=status_data["Balance"], pnl_percent=status_data["P&L(%)"], pnl_dollar=status_data["P&L($)"], last_check=status_data["Last Check"])
 
 def run():
-    port = int(os.environ.get("PORT", 3000))
+    port = int(os.environ.get("PORT", 5000))
     print(f"Running on port: {port}")
     app.run(host='0.0.0.0', port=port)
 
